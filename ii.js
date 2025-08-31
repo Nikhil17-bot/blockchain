70a569c9161cd25d870416f6ef47eef5c1937e22
Nikhil Kushwaha <kushwahanikhil431@gmail.com>
2025-08-31 18:58:50 +0530

    new

---
+++ ii.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+async function main() {
+  const CertificateRevocation = await hre.ethers.getContractFactory("CertificateRevocation");
+  const contract = await CertificateRevocation.deploy();
+
+  await contract.deployed();  // my new commit
+
+  console.log("CertificateRevocation deployed to:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("Error in deployment:", error);
+    process.exit(1);
+  });
